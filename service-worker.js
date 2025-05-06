4f8af91469e7a6237dd343048f70389415597824
Takayukimm <takayukimamabe@gmail.com>
2025-05-06 17:01:14 +0900

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,4 @@
+
+const CACHE_NAME='sapix-v2';
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(['./','./index.html','./manifest.json','./sapix_150.csv'])));});
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
